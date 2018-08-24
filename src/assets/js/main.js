85e0d552037b12e43e06b34ef820d5ff1936ce66
Ryan L <lee810317@hotmail.com.tw>
2018-08-24 12:39:05 +0800

    50%

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,37 @@
 $(function() {
+    $(".wrapper").tabs({
+        show: { effect: "fade", duration: 300 }
+    });
 
+
+    //mixer
+    var containerEl = document.querySelector('.mixerContainer');
+    var mixer = mixitup(containerEl, {
+        animation: {
+            effects: 'fade scale stagger(50ms)'
+        },
+        controls: {
+            toggleLogic: 'and'
+        },
+        load: {
+            filter: 'none',
+            sort: 'published-date:desc'
+        }
+    });
+    containerEl.classList.add('mixitup-ready');
+
+    mixer.show().then(function() {
+        mixer.configure({
+            animation: {
+                effects: 'fade scale'
+            }
+        });
+    });
+    // filter();
+
+    $('.events .PLText span.sort').text('活動網站');
+    $('.website .PLText span.sort').text('官方網站');
+    $('.banner .PLText span.sort').text('數位宣傳');
+    $('.video .PLText span.sort').text('動態影片');
+    $('.graphic .PLText span.sort').text('平面設計');
 });
